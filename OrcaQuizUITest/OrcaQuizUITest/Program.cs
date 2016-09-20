dfcfd9e22877965eeaae85ae7157bed4c2c16840
Lupux <ekblom.j.p@gmail.com>
2016-09-20 13:36:59 +0200

    Made Accesstests, They verify that if not loged in a user is redirected to sign in page.

--- OrcaQuizUITest/OrcaQuizUITest/Program.cs
+++ OrcaQuizUITest/OrcaQuizUITest/Program.cs
@@ -29,14 +29,54 @@ namespace OrcaQuizUITest
         }
 
         [Test]
+        public void AccessTests()
+        {
+            Console.WriteLine("Running access test to Dashboard");
+            // Test Access
+            string url = "http://localhost:27015/";
+            string testUrl;
+            string expectedUrl = "http://localhost:27015/Account/Login";
+
+            // Dashboard, Pass
+            testUrl = url + @"Dashboard/Index";
+            Assert.That( AccessTest(testUrl, expectedUrl), Does.Contain(expectedUrl));
+
+            // Manage User, Pass
+            testUrl = url + @"ManageUsers/Index";
+            Assert.That(AccessTest(testUrl, expectedUrl), Does.Contain(expectedUrl));
+
+            // Manage Group, Pass
+            testUrl = url + @"ManageGroups/Index";
+            Assert.That(AccessTest(testUrl, expectedUrl), Does.Contain(expectedUrl));
+
+            // Create test, Pass
+            testUrl = url + @"Admin/Test/Create";
+            Assert.That(AccessTest(testUrl, expectedUrl), Does.Contain(expectedUrl));
+
+            //Register, Fail
+            testUrl = url + @"Account/Register";
+            Assert.That(AccessTest(testUrl, expectedUrl), Does.Not.Contain(expectedUrl));
+
+            // TestSession, Pass
+            testUrl = url + @"TestSession/7013/1";
+            Assert.That(AccessTest(testUrl, expectedUrl), Does.Contain(expectedUrl));
+
+
+        }
+
+
+        [Test]
         public void ExecuteTest()
         {
             Console.WriteLine("Running Test!");
 
+
+
+
             // go to start page
             StartPageObject start = new StartPageObject();
-            
-            
+
+
             //string username = "admin@quiz.com";
             //string password = "P@ssw0rd";
 
@@ -54,6 +94,17 @@ namespace OrcaQuizUITest
         {
             PropertiesCollection.driver.Close();
             Console.WriteLine("Closing Test");
+        }
+
+        public string AccessTest(string testUrl, string expectedUrl)
+        {
+            string currentURL;
+            PropertiesCollection.driver.Navigate().GoToUrl(testUrl);
+            PropertiesCollection.driver.Manage().Window.Maximize();
+            currentURL = PropertiesCollection.driver.Url;
+           
+            Console.WriteLine($" current Url is {currentURL}");
+            return currentURL;
         }
     }
 }
